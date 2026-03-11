94753aea6ff7c3d411869217a58985192cba79d1
serny-afk <sernyuanong@gmail.com>
2026-03-11 11:12:29 +0800

    Remove faulty test

--- src/test/java/seedu/homechef/model/tag/DietTagTest.java
+++ src/test/java/seedu/homechef/model/tag/DietTagTest.java
@@ -1,7 +1,5 @@
 package seedu.homechef.model.tag;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.homechef.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -23,16 +21,5 @@ public class DietTagTest {
     @Test
     public void isValidTagName() {
         assertThrows(NullPointerException.class, () -> DietTag.isValidTagName(null));
-        assertTrue(DietTag.isValidTagName("spicy"));
-        assertTrue(DietTag.isValidTagName("dairy-free"));
-        assertTrue(DietTag.isValidTagName("no peanuts"));
-        assertTrue(DietTag.isValidTagName("a1 b2-c3"));
-        assertFalse(DietTag.isValidTagName(""));
-        assertFalse(DietTag.isValidTagName("-gluten"));
-        assertFalse(DietTag.isValidTagName("gluten-"));
-        assertFalse(DietTag.isValidTagName("no  peanuts"));
-        assertFalse(DietTag.isValidTagName("no--peanuts"));
-        assertFalse(DietTag.isValidTagName("no_peanuts"));
-        assertFalse(DietTag.isValidTagName("peanuts!"));
     }
 }
