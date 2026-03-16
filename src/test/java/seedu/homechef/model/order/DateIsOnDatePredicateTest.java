64aa39859df9b3010ded8862eeaa1b15ddf37727
serny-afk <sernyuanong@gmail.com>
2026-03-16 22:15:40 +0800

    Add test class for date predicate

---
+++ src/test/java/seedu/homechef/model/order/DateIsOnDatePredicateTest.java
@@ -0,0 +1,24 @@
+package seedu.homechef.model.order;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.homechef.testutil.TypicalOrders.ALICE;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+public class DateIsOnDatePredicateTest {
+
+    @Test
+    public void test_dateMatches_returnsTrue() {
+        LocalDate date = LocalDate.of(2026, 3, 26);
+        assertTrue(new DateIsOnDatePredicate(date).test(ALICE));
+    }
+
+    @Test
+    public void test_dateDoesNotMatch_returnsFalse() {
+        LocalDate date = LocalDate.of(2026, 3, 27);
+        assertFalse(new DateIsOnDatePredicate(date).test(ALICE));
+    }
+}
