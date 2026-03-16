6abeee2c2581a95f7483e27e64f6096e12220f37
serny-afk <sernyuanong@gmail.com>
2026-03-16 23:29:42 +0800

    Add tests to extend code coverage

--- src/test/java/seedu/homechef/model/order/DateIsOnDatePredicateTest.java
+++ src/test/java/seedu/homechef/model/order/DateIsOnDatePredicateTest.java
@@ -1,6 +1,8 @@
 package seedu.homechef.model.order;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.homechef.testutil.TypicalOrders.ALICE;
 
@@ -23,5 +25,28 @@ public class DateIsOnDatePredicateTest {
     public void test_dateDoesNotMatch_returnsFalse() {
         LocalDate date = LocalDate.of(2026, 3, 27);
         assertFalse(new DateIsOnDatePredicate(date).test(ALICE));
+    }
+
+    @Test
+    public void getDate_returnsConstructorDate() {
+        LocalDate date = LocalDate.of(2026, 3, 26);
+        assertEquals(date, new DateIsOnDatePredicate(date).getDate());
+    }
+
+    @Test
+    public void equals() {
+        LocalDate date = LocalDate.of(2026, 3, 26);
+        LocalDate otherDate = LocalDate.of(2026, 3, 27);
+
+        DateIsOnDatePredicate predicate = new DateIsOnDatePredicate(date);
+        DateIsOnDatePredicate samePredicate = new DateIsOnDatePredicate(date);
+        DateIsOnDatePredicate differentPredicate = new DateIsOnDatePredicate(otherDate);
+
+        assertEquals(predicate, predicate);
+        assertEquals(predicate, samePredicate);
+
+        assertNotEquals(predicate, null);
+        assertNotEquals(predicate, "not a predicate");
+        assertNotEquals(predicate, differentPredicate);
     }
 }
